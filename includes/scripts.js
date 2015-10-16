44f9736e2f312b7cae63dd6e59c6c810af46a0a4
elfirl <mike.bilter@gmail.com>
2015-10-15 22:23:48 -0400

    Set up, starting code
    Created the file, and started working on the code necessary for the
    assignment.

---
+++ includes/scripts.js
@@ -0,0 +1,11 @@
+var testVariable1 = 12; // This is a number
+var testVariable2 = "Mike"; // This is a string
+
+console.log (testVariable1);
+console.log (testVariable2);
+
+var myArray = ["String1", 3, NaN, "Netflix", 84]; // This is an Array, which is 0 indexed.
+var myCarObject = {make: "Mazda", model: "6", color: "black", year: 2011,}; // Objects use squiggly brackets.
+
+console.log (myArray[1]); // This returns the value at position 1, starting with 0.
+console.log (myCarObject["color"]); // This returns the value of the property in brackets.
